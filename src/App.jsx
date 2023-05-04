794ae8a79a71c810b9b3669dd354e88fbd41f14a
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-05-04 16:55:49 -0300

    styled page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 import './App.css'
+import Candy from './Components/Candy'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>🍭GetCandy🍬</h1>
-      <h3>Olá senhor (a):</h3>
+      <h1 className='title'>🍭GetCandy🍬</h1>
+      <div className='apresentation'>
+        <h3>Olá senhor (a):</h3>
+        <h3>Seja bem vindo!</h3>
+      </div>
 
+      <div className='tot'>
+        <h2>Total a pagar: R$ 0,00</h2>
+      </div>
+
+      <div className='container'>
+        <Candy
+        img= '/img/limao.jpg'
+        name ='Trufa Doce Desejo'
+        desc='Doce chocolate trufado com leite condesado e limão siciliano'
+        price='2,00'
+        />
+        <Candy
+        img= '/img/leite.jpg'
+        name ='Doce de Leite dos Deuses'
+        desc='Doce de doce de leite com recheio de maracujá com sementes cristalizadas de maracujá'
+        price='3,00'
+        />
+        <Candy
+        img= '/img/laranjamel.jpg'
+        name ='Paletta Dolce Gusto'
+        desc='Doce de bolacha com paleta italiana de (morango, limão ou laranja) e mel'
+        price='4,50'
+        />
+      </div>
+
+      <footer><span>Orgulhosamente Produzido por Gabriel Moreira</span></footer>
     </>
   )
 }
--- src/Components/Candy/index.jsx
+++ src/Components/Candy/index.jsx
@@ -1,9 +1,16 @@
+import "./Candy.css"
 
 
-
-const Candy = () => {
+const Candy = ({img, name, desc, price, quant, valuepay}) => {
     return(
-        <></>
+        <div className="candy">
+            <p className="price">Quantidade na sacola: {quant}</p>
+            <p className="price">Valor pago: {valuepay}</p>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>{desc}</p>
+            <p className="price">R${price}</p>
+        </div>
     )
 }
 
